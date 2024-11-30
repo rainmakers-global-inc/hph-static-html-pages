c68480ce3c082fadb88680c78787db0b5545933c
Sujoy Kairi <sujoy@esolzmail.com>
2024-11-30 10:20:40 +0530

    home, location,assesment page

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    $('.homebanner').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode:true,
+        centerPadding: '0px',
+        infinite: true,
+        // autoplaySpeed: 5000,
+        // autoplay: true
+        
+    });
+})
